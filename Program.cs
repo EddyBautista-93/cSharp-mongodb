3f624951ca5542a22ab7ac3e82ff8426ffa3d527
EddyBautista <bautista.j.eddy@gmail.com>
2022-06-22 15:13:27 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using MongoDB.Driver;
+using Microsoft.Extensions.Configuration;
+
+namespace cSharp_mongodb
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                        .AddUserSecrets<Program>()
+                        .Build();
+
+            string mongoString = config["MONGOSTRING"];
+
+            MongoClient dbClient = new MongoClient(mongoString);
+
+            var dbList = dbClient.ListDatabases().ToList();
+
+            Console.WriteLine(" Databases on this server:");
+
+            foreach (var db in dbList)
+            {
+                Console.WriteLine(db);
+            }
+        }
+    }
+}
